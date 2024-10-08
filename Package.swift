3ca6669b12a7b0e16e79ddf5c914d7aabe91d8b0
Isa <ileitelima@apple.com>
2024-10-08 15:07:10 -0700

    update to swift 6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.0
 //
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 // Swift 5.9 is available from Xcode 15.0.
